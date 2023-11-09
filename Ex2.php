4f2716fe751c65184d9537202c8857818e6bd306
Runkana Durga Prasad <111352968+DurgaPrasad-R@users.noreply.github.com>
2023-11-09 07:03:15 +0530

    Update Ex2.php

--- Ex2.php
+++ Ex2.php
@@ -1,47 +1,30 @@
-<html>
-<body>
-<!-- Array in PHP
-An array stores multiple values in one single variable
-In PHP, there are three kinds of arrays:
-- Numeric array
-- Associative array: an array where each ID key is associated with a value
-- Multidimensional array -->
 <?php
-function myFunction()
-{
-echo "Hello, We see Different types of Arrays Here:<br>";
+echo "Welcome, Let's see various types of Arrays";
+echo "<br>";
+echo "1. Numeric Arrays:";
+echo "<br>";
+$arr = array('Rose','Daisy','Jasmine');
+echo "Flowers:".$arr[0].",".$arr[1].",".$arr[2];
+echo "<br>";
+echo "2. Associative Arrays:";
+echo "<br>";
+$flower_shop = array('ROSE' => 12,'DAISY' => 15, 'Jasmine' => 18);
+echo "List of Flowers and their Costs:";
+echo "<br>";
+echo "ROSE:".$flower_shop['ROSE'];
+echo "<br>";
+echo 'DAISY:'.$flower_shop['DAISY'];
+echo "<br>";
+echo 'JASMINE:'.$flower_shop['Jasmine'];
+echo "<br>";
+echo "Looping:";
+echo "<br>";
+foreach($flower_shop as $x=>$x_value){
+    echo $x.":".$x_value."<br>";
 }
-// Function call
-myFunction();
-$flower_shop = array ("rose", "daisy","orchid");echo "Flowers:
-".$flower_shop[0].",".$flower_shop[1].", ".$flower_shop[2]."";
-echo "<br>";
-$flower_shop = array ( "rose" => "5.00", "daisy" => "4.00", "orchid"
-=> "2.00" );
-// Display the array values
-echo "\n";
-
-echo "rose costs" .$flower_shop['rose'].",daisy costs
-".$flower_shop['daisy'].",and orchid
-costs ".$flower_shop['orchid']."";
-echo "<br>";
-foreach($flower_shop as $x=>$x_value) {
-    echo "Flower=" . $x . ", Value=" . $x_value;
-    echo "<br>";
-    }
-    $flower_shop = array(
-        "rose" => array( "5.00", "7 items", "red" ),
-        "daisy" => array( "4.00", "3 items", "blue" ),
-        "orchid" => array( "2.00", "1 item", "white" ),
-        );
-        echo "rose costs ".$flower_shop['rose'][0].
-        ", and you get ".$flower_shop['rose'][1].".<br>";
-        echo "daisy costs ".$flower_shop['daisy'][0].
-        ", and you get ".$flower_shop['daisy'][1].".<br>";
-        echo "orchid costs ".$flower_shop['orchid'][0].
-        ", and you get ".$flower_shop['orchid'][1].".<br>";
-
+echo "3.Multi-Dimensional Arrays:<br>";
+$flower_shop_costs = array(
+    "Rose" => array(10,5,'red')
+);
+echo "Rose costs: ".$flower_shop_costs["Rose"][0]." and you will get:".$flower_shop_costs["Rose"][1]." items";
 ?>
-
-</body>
-</html>
